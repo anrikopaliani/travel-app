7f5e5e6980e9fe0950fa2150f146347cd006bebf
kopala123 <anri.kopaliani.1@btu.edu.ge>
2024-01-19 17:54:58 +0400

    style:finish footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,7 @@ import Button from "./Button";
 const Navbar = () => {
   return (
     <nav className=" flexBetween max-container padding-container relative z-30 py-5">
-      <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
-      </Link>
+      {/* <Link href="/">Camp</Link> */}
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
           <Link
@@ -28,14 +26,6 @@ const Navbar = () => {
           variant="btn_dark_green"
         />
       </div>
-
-      <Image
-        src="/menu.svg"
-        alt="menu"
-        width={32}
-        height={32}
-        className="inline-block cursor-pointer lg:hidden"
-      />
     </nav>
   );
 };
